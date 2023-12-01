2567887c618f7940db1be4658e24ba94f7243560
Alessandro Branchetti <brk.alessandro@gmail.com>
2023-12-01 14:19:13 +0100

    added Home page

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello!");
+const timeContainer = document.getElementById("time-container");
+
+const BIRTH_DAY = "05-08-1997";
+const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
+const intlNumberFormatter = new Intl.NumberFormat("en-US");
+
+setInterval(() => {
+  const now = new Date();
+  const difference = Math.floor(
+    (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
+  );
+
+  timeContainer.innerText = intlNumberFormatter.format(difference);
+}, 1000);
